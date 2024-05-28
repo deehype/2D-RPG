53aa350c12ac348d04eb413aa03cdb67bee71145
deehype <164973662+deehype@users.noreply.github.com>
2024-05-28 12:19:30 +0900

    C3. 캐릭터 공격

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,30 +1,134 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
     private Animator animator;
     private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rigidbody2d;
+    private AudioSource audioSource;
+
+    public AudioClip JumpClip;
+
+    private bool justJumped = false;
 
     public float Speed = 4f;
+    public float JumpPower = 6f;
+
+    private bool isFloor;
+
+    public GameObject AttackObj;
+    public float AttackSpeed = 3f;
+    public AudioClip AttackClip;
+
+    private bool justAttack, justJump;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
     }
 
-    private void OnAnimatorMove()
+    void Update()
     {
-        //이동
+        Move();
+        JumpCheck();
+        Jump();
+        Attack();
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
+    private void Move()
+    {
+        //이동 GetKey는 키보드를 누른 상태마다 실행
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(Vector3.right * Speed * Time.deltaTime);
+            animator.SetBool("Move", true);
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(Vector3.left * Speed * Time.deltaTime);
+            animator.SetBool("Move", true);
+        }
+        else
+        {
+            animator.SetBool("Move", false);
+        }
+
+        //좌우 이동에 따른 반전 GetKeyDown은 키보드를 누르고 한 번 실행
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            isFloor = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            isFloor = false;
+        }
+    }
+
+    private void JumpCheck()
+    {
+        if (isFloor)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                justJump = true;
+            }
+        }
+    }
+
+    private void Jump()
+    {
+        if (justJump)
+        {
+            justJump = false;
+
+            rigidbody2d.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
+            animator.SetTrigger("Jump");
+            audioSource.PlayOneShot(JumpClip);
+        }
+    }
+
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            animator.SetTrigger("Attack");
+            audioSource.PlayOneShot(AttackClip);
+
+            if (spriteRenderer.flipX)
+            {
+                GameObject obj = Instantiate(AttackObj, transform.position, Quaternion.Euler(0, 180f, 0));
+                obj.GetComponent<Rigidbody2D>().AddForce(Vector2.left * AttackSpeed, ForceMode2D.Impulse);
+                Destroy(obj, 3f);
+            }
+            else
+            {
+                GameObject obj = Instantiate(AttackObj, transform.position, Quaternion.Euler(0, 0, 0));
+                obj.GetComponent<Rigidbody2D>().AddForce(Vector2.right * AttackSpeed, ForceMode2D.Impulse);
+                Destroy(obj, 3f);
+            }
         }
     }
 }
